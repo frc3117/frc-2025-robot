fa10c55eb19bfefe119cef6b0304c3444300e58f
mickjosh <felix_roux@hotmail.com>
2025-01-25 13:51:00 -0500

    Swerve Finally Working!

--- robot/robot.py
+++ robot/robot.py
@@ -14,15 +14,15 @@ class Robot(RobotBase):
     def robotInit(self):
         super().robotInit()
 
-        Input.add_axis('horizontal',
+        self.__h = Input.add_axis('horizontal',
                        0,
-                       XboxControllerInput.LEFT_JOYSTICK_X,
+                       XboxControllerInput.LEFT_JOYSTICK_Y,
                        deadzone=0.02,
                        axis_filter=SlewRateLimiter(1),
                        axis_transform=PowerTransform(1.5))
         Input.add_axis('vertical',
                        0,
-                       XboxControllerInput.LEFT_JOYSTICK_Y,
+                       XboxControllerInput.LEFT_JOYSTICK_X,
                        inverted=True,
                        deadzone=0.02,
                        axis_filter=SlewRateLimiter(1),
@@ -40,37 +40,35 @@ class Robot(RobotBase):
                                                deadzone=0.02,
                                                axis_transform=PowerTransform(1.5)),
                        axis_filter=SlewRateLimiter(1.75))
-                       
 
         swerveModule = [
-            SwerveModule(drive_motor=WPI_CANSparkFlex(2, True, brake=True),
+            SwerveModule(drive_motor=WPI_CANSparkFlex(2, True, brake=True, inverted=True),
                          steering_motor=WPI_CANSparkMax(1, True, brake=True),
                          steering_encoder=Encoder(DutyCycleEncoder(0), 0, False),
                          steering_controller=PID(1, 0, 0),
-                         steering_offset=0.0512,
+                         steering_offset=0.548,
                          position=Vector2(-10.875, 13.375)),
 
             SwerveModule(drive_motor=WPI_CANSparkFlex(8, True, brake=True),
-                         steering_motor=WPI_CANSparkMax(7, True, brake=True, inverted=True),
-                         steering_encoder=Encoder(DutyCycleEncoder(1), 0, False),
-                         steering_controller=PID(1, 0, 0),
-                         steering_offset=0.0512,
-                         position=Vector2(10.875, 13.375)),
-
-            SwerveModule(drive_motor=WPI_CANSparkFlex(6, True, brake=True),
-                         steering_motor=WPI_CANSparkMax(5, True, brake=True, inverted=True),
-                         steering_encoder=Encoder(DutyCycleEncoder(2), 0, False),
-                         steering_controller=PID(1, 0, 0),
-                         steering_offset=0,
-                         position=Vector2(10.875, -13.375)),
-
-            SwerveModule(drive_motor=WPI_CANSparkFlex(4, True, brake=True),
-                         steering_motor=WPI_CANSparkMax(3, True, brake=True, inverted=True),
+                         steering_motor=WPI_CANSparkMax(7, True, brake=True),
                          steering_encoder=Encoder(DutyCycleEncoder(3), 0, False),
                          steering_controller=PID(1, 0, 0),
-                         steering_offset=0,
-                         position=Vector2(-10.875, -13.375))
+                         steering_offset=0.821,
+                         position=Vector2(-10.875, -13.375)),
 
+            SwerveModule(drive_motor=WPI_CANSparkFlex(6, True, brake=True),
+                         steering_motor=WPI_CANSparkMax(5, True, brake=True),
+                         steering_encoder=Encoder(DutyCycleEncoder(2), 0, False),
+                         steering_controller=PID(1, 0, 0),
+                         steering_offset=0.389,
+                         position=Vector2(10.875, -13.375)),
+
+            SwerveModule(drive_motor=WPI_CANSparkFlex(4, True, brake=True, inverted=True),
+                         steering_motor=WPI_CANSparkMax(3, True, brake=True),
+                         steering_encoder=Encoder(DutyCycleEncoder(1), 0, False),
+                         steering_controller=PID(1, 0, 0),
+                         steering_offset=0.001,
+                         position=Vector2(10.875, 13.375))
         ]
 
         swerve = SwerveDrive(swerveModule, imu=ADIS16448_IMU(ADIS16448_IMU.IMUAxis.kZ, SPI.Port.kMXP, ADIS16448_IMU.CalibrationTime._1s), start_heading=math.pi)
