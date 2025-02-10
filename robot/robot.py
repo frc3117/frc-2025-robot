1f1e8f451ce44145a85d26dc5198473520c6581b
mickjosh <felix_roux@hotmail.com>
2025-02-10 14:32:59 -0500

    Adding Part1 Of Elevator

--- robot/robot.py
+++ robot/robot.py
@@ -9,7 +9,6 @@ from frctools.input import Input, XboxControllerInput, PowerTransform
 from frctools.vision.apriltags import AprilTagsReefscapeField
 
 from robot2025 import Climber, Conveyor, CoralOuttake, Elevator
-from robot2025.driver_station import ReefSelector
 
 from wpilib import DutyCycleEncoder, ADIS16448_IMU, SPI
 
@@ -22,14 +21,14 @@ class Robot(RobotBase):
                        0,
                        XboxControllerInput.LEFT_JOYSTICK_Y,
                        deadzone=0.02,
-                       axis_filter=SlewRateLimiter(1),
+                       axis_filter=SlewRateLimiter(100),
                        axis_transform=PowerTransform(1.5))
         Input.add_axis('vertical',
                        0,
                        XboxControllerInput.LEFT_JOYSTICK_X,
                        inverted=True,
                        deadzone=0.02,
-                       axis_filter=SlewRateLimiter(1),
+                       axis_filter=SlewRateLimiter(100),
                        axis_transform=PowerTransform(1.5))
 
         Input.create_composite_axis('rotation',
@@ -43,39 +42,40 @@ class Robot(RobotBase):
                                                XboxControllerInput.LEFT_TRIGGER,
                                                deadzone=0.02,
                                                axis_transform=PowerTransform(1.5)),
-                       axis_filter=SlewRateLimiter(1.75))
+                       axis_filter=SlewRateLimiter(100))
 
         swerveModule = [
             SwerveModule(drive_motor=WPI_CANSparkFlex(2, True, brake=True, inverted=True),
                          steering_motor=WPI_CANSparkMax(1, True, brake=True),
                          steering_encoder=Encoder(DutyCycleEncoder(0), 0, False),
-                         steering_controller=PID(1, 0, 0),
+                         steering_controller=PID(0.3, 0, 0),
                          steering_offset=0.548,
                          position=Vector2(-10.875, 13.375)),
 
             SwerveModule(drive_motor=WPI_CANSparkFlex(8, True, brake=True),
                          steering_motor=WPI_CANSparkMax(7, True, brake=True),
                          steering_encoder=Encoder(DutyCycleEncoder(3), 0, False),
-                         steering_controller=PID(1, 0, 0),
+                         steering_controller=PID(0.3, 0, 0),
                          steering_offset=0.821,
                          position=Vector2(-10.875, -13.375)),
 
             SwerveModule(drive_motor=WPI_CANSparkFlex(6, True, brake=True),
                          steering_motor=WPI_CANSparkMax(5, True, brake=True),
                          steering_encoder=Encoder(DutyCycleEncoder(2), 0, False),
-                         steering_controller=PID(1, 0, 0),
+                         steering_controller=PID(0.3, 0, 0),
                          steering_offset=0.389,
                          position=Vector2(10.875, -13.375)),
 
             SwerveModule(drive_motor=WPI_CANSparkFlex(4, True, brake=True, inverted=True),
                          steering_motor=WPI_CANSparkMax(3, True, brake=True),
                          steering_encoder=Encoder(DutyCycleEncoder(1), 0, False),
-                         steering_controller=PID(1, 0, 0),
+                         steering_controller=PID(0.3, 0, 0),
                          steering_offset=0.001,
                          position=Vector2(10.875, 13.375))
         ]
 
         swerve = SwerveDrive(swerveModule, imu=ADIS16448_IMU(ADIS16448_IMU.IMUAxis.kZ, SPI.Port.kMXP, ADIS16448_IMU.CalibrationTime._1s), start_heading=math.pi)
+        swerve.set_cosine_compensation(True)
         self.add_component('Swerve', swerve)
 
         climber = Climber()
@@ -87,8 +87,8 @@ class Robot(RobotBase):
         coral_outtake = CoralOuttake()
         self.add_component('CoralOuttake', coral_outtake)
 
-        elevator = Elevator()
-        self.add_component('Elevator', elevator)
+        #elevator = Elevator()
+        #self.add_component('Elevator', elevator)
 
     def disabledExit(self):
         super().disabledExit()
--- robot/robot2025/elevator.py
+++ robot/robot2025/elevator.py
@@ -1,6 +1,41 @@
-from frctools import Component
+from frctools import Component, Timer, CoroutineOrder
+from frctools.sensor import Encoder
+from frctools.controll import PID
+
+import wpiutil
 
 
 class Elevator(Component):
-    def __init__(self):
+    def __init__(self, elevator_motor, elevator_encoder: Encoder, controller: PID):
         super().__init__()
+
+        self.__motor = elevator_motor
+        self.__encoder = elevator_encoder
+        self.__controller = controller
+
+        self.__target_height = 0.
+
+        self.__control_coroutine = None
+
+    def update(self):
+        self.__control_coroutine = Timer.start_coroutine_if_stopped(self.__control_loop__, self.__control_coroutine)
+
+    def __control_loop__(self):
+        while True:
+            yield None
+
+    def set_target_height(self, height: float):
+        self.__target_height = height
+
+    def get_current_height(self) -> float:
+        return self.__encoder.get()
+
+    def hold_height(self):
+        self.set_target_height(self.get_current_height())
+
+    def wait_for_height(self):
+        pass
+
+    def initSendable(self, builder: wpiutil.SendableBuilder):
+        builder.addDoubleProperty('height', self.get_current_height, lambda v: None)
+        builder.addDoubleProperty('target_height', lambda: self.__target_height, self.set_target_height)
